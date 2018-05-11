226c6a5574ade8e2d924242e69a2214e6b50b1dd
yeskee <stayallnight@163.com>
2018-05-11 19:42:05 +0800

    加入了函数文件script和测试文件func

---
+++ bili_script.py
@@ -0,0 +1,27 @@
+import time
+
+def TTS(x):
+	#name of the function means Time to Stamp
+	#return a int
+	timeArray = time.strptime(x, '%Y-%m-%d %H:%M:%S')
+	timeStamp = time.mktime(timeArray)
+	return timeStamp
+
+
+def printmes(start,end,low,high):
+	#本次执行开始时间，结束时间，区间起始和终止位置.
+	#文件名这样写也能成功读取
+	log_name = 'D:\codes\python\logs/running-date-'+time.strftime("%Y-%m-%d", time.localtime())+'.txt'
+	f = open(log_name, 'a')
+	f.write('===============================\n')
+	f.write('\n本次运行开始时间：'+str(start)+'\n')
+	f.write('本次运行结束时间：'+str(end)+'\n')
+
+	f.write('执行起始号码位置：'+str(low)+'\n')
+	f.write('执行终止号码位置：'+str(high)+'\n')
+	f.write('实际终止号码位置：'+str(high-1)+'\n')
+
+	average = round((TTS(end)-TTS(start))/(int(high)-int(low)),2)
+	f.write('平均每条读取用时：'+str(average)+'s\n')
+	f.write('\n===============================\n\n')
+	pass
---
+++ funcs_test.py
@@ -0,0 +1,12 @@
+import time
+import bili_script
+
+
+start = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+time.sleep(5)
+end = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+bili_script.printmes(start,end,1,4)
+#1525940053
+
+
+
